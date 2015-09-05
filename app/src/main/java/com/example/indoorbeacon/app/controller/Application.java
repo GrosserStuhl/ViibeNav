570cc7821059135d4c444e37f6c003e91d73dae1
Pinguinmann <Tom.Aussenhofer@gmx.de>
2015-09-05 13:43:45 +0200

    - jetzt wird gleich gemerged

--- app/src/main/java/com/example/indoorbeacon/app/controller/Application.java
+++ app/src/main/java/com/example/indoorbeacon/app/controller/Application.java
@@ -1,18 +1,10 @@
 package com.example.indoorbeacon.app.controller;
 
 import android.content.Intent;
-import android.os.Handler;
 import android.view.View;
-import android.widget.TextView;
 
-import com.example.indoorbeacon.app.R;
 import com.example.indoorbeacon.app.model.InfoActivity;
 import com.example.indoorbeacon.app.model.Measurement;
-import com.example.indoorbeacon.app.model.OnyxBeacon;
-import com.example.indoorbeacon.app.model.RadioMap;
-
-import java.util.Iterator;
-import java.util.Map;
 
 /**
  * Created by TomTheBomb on 23.06.2015.
@@ -21,27 +13,7 @@ public class Application {
 
     public MainActivity main;
 
-    /*
-    GUI elements
-    Consider the UI is divided into three layers:
-     ________________________________________
-    |                                        |
-    |                 Overview               |
-    |________________________________________|
-    |                                        |
-    |               RSSIs around             |
-    |________________________________________|
-    |                                        |
-    |           Navigation elements          |
-    |________________________________________|
-     */
 
-    // Layer 1
-    TextView totalAnchor,lastX,lastY,lastEtage;
-    // Layer 2
-    TextView anzahlBeaconView,tempRSSIsView;
-
-    Handler calcMediansHandler;
     Measurement measurement;
 
 
@@ -49,58 +21,9 @@ public class Application {
     Application(MainActivity main){
         this.main = main;
         measurement = new Measurement();
-
-        // initialize GUI elemnts
-        initGUI();
     }
 
-    private void initGUI(){
-        // Layer 1
-        totalAnchor = (TextView) main.findViewById(R.id.totalAnchor);
-        lastX = (TextView) main.findViewById(R.id.lastX);
-        lastY = (TextView) main.findViewById(R.id.lastY);
-        lastEtage = (TextView) main.findViewById(R.id.lastEtage);
 
-        // Layer 2
-        anzahlBeaconView = (TextView) main.findViewById(R.id.anzahlBeaconFeld);
-        tempRSSIsView = (TextView) main.findViewById(R.id.tempRSSIFeld);
-
-
-    }
-
-    /**
-     * By invoking this method you start median measurement for the beacons found nearby.
-     * It will only start median measurement for the beacons already listed in the onyxBeaconHashMap.
-     */
-    public void startMeasurement(View view){
-        calcMediansHandler.sendEmptyMessage(0);
-    }
-
-    public void updateLayer1(){
-        totalAnchor.setText("" + RadioMap.size());
-        lastX.setText(""+RadioMap.getRadioMap().getLastAnchor().getCoordinate().getX());
-        lastY.setText(""+RadioMap.getRadioMap().getLastAnchor().getCoordinate().getY());
-        lastEtage.setText("" + RadioMap.getRadioMap().getLastAnchor().getCoordinate().getFloor());
-    }
-
-    public void updateLayer2(){
-        anzahlBeaconView.setText(""+OnyxBeacon.beaconMap.size());
-        String tempRSSIs = "";
-        Iterator it = OnyxBeacon.beaconMap.entrySet().iterator();
-        int count = 0;
-        while (it.hasNext()) {
-            count++;
-            Map.Entry<String,OnyxBeacon> pair = (Map.Entry)it.next();
-            if(tempRSSIs.isEmpty())
-                tempRSSIs += "" + pair.getValue().getRssi();
-            else {
-                tempRSSIs += "|" + pair.getValue().getRssi();
-                if((count % 4) == 0)
-                    tempRSSIs += System.lineSeparator();
-            }
-        }
-        tempRSSIsView.setText(tempRSSIs);
-    }
 
 
     public void clickInfo(View view){
