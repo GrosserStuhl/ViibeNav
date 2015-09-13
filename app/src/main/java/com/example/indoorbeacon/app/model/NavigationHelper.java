937ce44cc3a58cd05d2342f0c247641c89e620c1
Dimitri <paralysekid@gmail.com>
2015-09-14 00:12:06 +0200

    Kleine Änderung des Ausgabetextes für die letzte Range

--- app/src/main/java/com/example/indoorbeacon/app/model/NavigationHelper.java
+++ app/src/main/java/com/example/indoorbeacon/app/model/NavigationHelper.java
@@ -347,7 +347,7 @@ public class NavigationHelper {
         int index = ranges.indexOf(currentRange);
 
         Range nextRange;
-        if(index != ranges.size()-1) {
+        if (index != ranges.size() - 1) {
             nextRange = ranges.get(index + 1);
             ArrayList<String> environmentalInfo = nextRange.getEnvironmentalInfos();
             instructionTexts = new ArrayList<>();
@@ -361,8 +361,8 @@ public class NavigationHelper {
             tts.speakList(instructionTexts, 0);
         } else {
             ArrayList<String> alternative = new ArrayList<>();
-            alternative.add("Es gibt keine nächste Anweisung, danach haben Sie ihr Ziel erreicht.");
-            tts.speakList(alternative,0);
+            alternative.add("Danach haben Sie ihr Ziel erreicht, es gibt keine nächste Anweisung.");
+            tts.speakList(alternative, 0);
         }
 //        ArrayList<String> environmentalInfo = currentRange.getEnvironmentalInfos();
 //        ArrayList<String> strings = new ArrayList<>();
@@ -382,7 +382,7 @@ public class NavigationHelper {
         int index = ranges.indexOf(currentRange);
 
         Range previousRange;
-        if(index != 0) {
+        if (index != 0) {
             previousRange = ranges.get(index - 1);
             ArrayList<String> environmentalInfo = previousRange.getEnvironmentalInfos();
             instructionTexts = new ArrayList<>();
@@ -397,7 +397,7 @@ public class NavigationHelper {
         } else {
             ArrayList<String> alternative = new ArrayList<>();
             alternative.add("Die Navigation hat hier begonnen, es gibt noch keine vorherige Anweisung.");
-            tts.speakList(alternative,0);
+            tts.speakList(alternative, 0);
         }
     }
 
